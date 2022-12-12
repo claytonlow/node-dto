be966123bd5d2bf4970ac72ca514d5d65b455a29
claytonlow <claytonlow@gmail.com>
2022-12-13 10:39:00 +1300

    initial commit

---
+++ src/utils/validators/validateSchema.js
@@ -0,0 +1,133 @@
+const ValidateException = require('../../exceptions/ValidateException');
+const AvailableTypes = require('../types');
+const exceptionTypes = {
+  Enum: 'Enum',
+  Array: 'Array',
+  Object: 'Object',
+  JSON: 'JSON'
+};
+
+const validateProp = {
+  Enum: 'enumOps',
+  Object: 'schema',
+  Array: 'itemsType',
+};
+
+function validateExceptions(
+  row,
+  validateArray,
+  validateEnum,
+  schema,
+  AvailableTypes
+) {
+  const prop = validateProp[row.type];
+
+  if (prop) {
+    if (row.type && (!row[prop] || !row[prop].length))
+      throw new ValidateException(
+        `Prop '${prop}' is required when you choose type '${row.type}'!`
+      );
+
+    if (row.type === exceptionTypes.Object) {
+      validateSchema(
+        row.schema,
+        validateArray,
+        validateEnum,
+        schema,
+        AvailableTypes
+      );
+    }
+
+    if (row.type === exceptionTypes.Array) {
+
+      if (row.itemsType === exceptionTypes.Array) {
+        throw new ValidateException(
+          "Can't cambine type Array with itemsType Array!"
+        );
+      }
+
+      validateType(AvailableTypes, [row.itemsType]);
+
+      validateExceptions(
+        {
+          ...row,
+          type: row.itemsType,
+        },
+        validateArray,
+        validateEnum,
+        schema,
+        AvailableTypes
+      );
+    }
+
+    if (row.type === exceptionTypes.Enum) {
+      validateArray(row.enumOps);
+    }
+
+    
+  }
+
+  return;
+}
+
+function validateSchema(
+  params,
+  validateArray,
+  validateEnum,
+  schema,
+  AvailableTypes
+) {
+  validateArray(params);
+  validateArray(params, AvailableTypes.Object, 'Object');
+
+  for (let i = 0; i < params.length; i++) {
+    const row = params[i];
+    const keys = Object.keys(row);
+
+    for (const key of schema) {
+      if (key !== 'required') {
+        validateEnum(key, keys, `Prop '${key}' is missing on schema index ${i}!`);
+      }
+    }
+
+    if (row.defaultValue) {
+      result =
+        row.type === 'Enum'
+          ? AvailableTypes[row.type](row.defaultValue, row.enumOps)
+          : AvailableTypes[row.type](row.defaultValue);
+      if (!result) {
+        throw new ValidateException(
+          `Value of the Field defaultValue is not of the type '${row.type}' on schema index ${i}!`
+        );
+      }
+    }
+
+    validateExceptions(
+      row,
+      validateArray,
+      validateEnum,
+      schema,
+      AvailableTypes
+    );
+  }
+
+  validateType(
+    AvailableTypes,
+    params.map((dt) => dt.type)
+  );
+}
+
+function validateType(AvailableTypes, types) {
+  const availableTypes = Object.keys(AvailableTypes);
+
+  types.forEach((type) => {
+    if (!availableTypes.includes(type)) {
+      throw new ValidateException(`${type} was not recognized!`);
+    }
+  });
+}
+
+module.exports = {
+  exceptionTypes,
+  validateSchema,
+};
